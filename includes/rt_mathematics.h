4e29da9670ce2711fce73b85b8f9c1575abe5af6
aygaoua <102914808+aygaoua@users.noreply.github.com>
2024-02-16 17:32:37 +0100

    adding lot of tools to the matrix opirations

--- includes/rt_mathematics.h
+++ includes/rt_mathematics.h
@@ -6,7 +6,7 @@
 /*   By: azgaoua <azgaoua@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 13:36:31 by hlaadiou          #+#    #+#             */
-/*   Updated: 2024/02/16 14:43:43 by azgaoua          ###   ########.fr       */
+/*   Updated: 2024/02/16 17:21:58 by azgaoua          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,15 @@ typedef struct s_vector
 	double	y;
 	double	z;
 }	t_vector;
+
+float 		cofactor(t_matrix *a, int r, int c);
+float		minor(t_matrix *a, int r, int c);
+float		mtx_determinant(t_matrix *a);
+t_matrix	*identity_mtx(int size);
+t_matrix	*mtx_create(int size);
+t_matrix	*mtx_multiply(t_matrix *a,t_matrix *b);
+t_matrix	*submatrix(t_matrix *a, int r, int c);
+t_matrix 	*mtx_transpose(t_matrix  *a);
+t_matrix	*inverse(t_matrix *a);
 
 #endif
--- sources/rt_mathematics/mtx.c
+++ sources/rt_mathematics/mtx.c
@@ -6,7 +6,7 @@
 /*   By: azgaoua <azgaoua@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 10:24:48 by azgaoua           #+#    #+#             */
-/*   Updated: 2024/02/16 15:26:01 by azgaoua          ###   ########.fr       */
+/*   Updated: 2024/02/16 17:31:39 by azgaoua          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,36 @@ t_matrix	*mtx_create(int size)
 		mtx->mtx[i] = (float *)malloc(sizeof(float) * size);
 	return (mtx);
 }
+
+t_matrix	*submatrix(t_matrix *a, int r, int c)
+{
+	t_matrix	*sub;
+	int			i;
+	int			j;
+	int			row;
+	int			col;
+
+	sub = mtx_create(a->size - 1);
+	i = -1;
+	row = 0;
+	while (++i < a->size)
+	{
+		if (i == r)
+			continue ;
+		j = -1;
+		col = 0;
+		while (++j < a->size)
+		{
+			if (j == c)
+				continue ;
+			sub->mtx[row][col] = a->mtx[i][j];
+			col++;
+		}
+		row++;
+	}
+	return (sub);
+}
+
 
 t_matrix	*identity_mtx(int size)
 {
@@ -75,12 +105,59 @@ t_matrix	*mtx_multiply(t_matrix *a,t_matrix *b)
 	return (c);
 }
 
-float	mtx_determinant(t_matrix a)
+float	mtx_determinant(t_matrix *a)
 {
 	float	dtr;
+	int		i;
 
+	i = 0;
 	dtr = 0;
+	if (a->size == 2)
+		dtr = (a->mtx[0][0] * a->mtx[1][1]) - (a->mtx[0][1] * a->mtx[1][0]);
+	else
+	{
+		while (i < a->size)
+		{
+			dtr += a->mtx[0][i] * cofactor(a, 0, i);
+			i++;
+		}
+	}
 	return (dtr);
+}
+
+t_matrix	*inverse(t_matrix *a)
+{
+	t_matrix	*b;
+	int			row;
+	int			col;
+	float		c;
+	
+	if (mtx_determinant(a) == 0)
+		return (NULL);
+	row = 0;
+	b = mtx_create(a->size);
+	while (row < a->size)
+	{
+		col = 0;
+		while (col < a->size)
+		{
+			c = cofactor(a, row, col);
+			b->mtx[col][row] = c / mtx_determinant(a);
+			col++;
+		}
+		row++;
+	}
+	return (b);
+}
+
+float	minor(t_matrix *a, int r, int c)
+{
+	return (mtx_determinant(submatrix(a, r, c)));
+}
+
+float cofactor(t_matrix *a, int r, int c)
+{
+	return (((r + c) % 2 == 0) ? minor(a, r, c) : (minor(a, r, c) * -1));
 }
 
 t_matrix *mtx_transpose(t_matrix  *a)
@@ -108,21 +185,27 @@ int main()
 
 	A = mtx_create(4);
 	B = mtx_create(4);
-	A->mtx[0][0] = 1;
-	A->mtx[0][1] = 2;
-	A->mtx[0][2] = 3;
-	A->mtx[0][3] = 4;
-	A->mtx[1][0] = 5;
-	A->mtx[1][1] = 6;
-	A->mtx[1][2] = 7;
-	A->mtx[1][3] = 8;
-	A->mtx[2][0] = 9;
-	A->mtx[2][1] = 8;
-	A->mtx[2][2] = 7;
-	A->mtx[2][3] = 6;
-	A->mtx[3][0] = 5;
-	A->mtx[3][1] = 4;
-	A->mtx[3][2] = 3;
+	/*
+	| 9 | 3 | 0 | 9 |
+	| -5 | -2 | -6 | -3 |
+	| -4 | 9 | 6 | 4 |
+	| -7 | 6 | 6 | 2 |
+	*/
+	A->mtx[0][0] = 9;
+	A->mtx[0][1] = 3;
+	A->mtx[0][2] = 0;
+	A->mtx[0][3] = 9;
+	A->mtx[1][0] = -5;
+	A->mtx[1][1] = -2;
+	A->mtx[1][2] = -6;
+	A->mtx[1][3] = -3;
+	A->mtx[2][0] = -4;
+	A->mtx[2][1] = 9;
+	A->mtx[2][2] = 6;
+	A->mtx[2][3] = 4;
+	A->mtx[3][0] = -7;
+	A->mtx[3][1] = 6;
+	A->mtx[3][2] = 6;
 	A->mtx[3][3] = 2;
 	B->mtx[0][0] = -2;
 	B->mtx[0][1] = 1;
@@ -141,31 +224,66 @@ int main()
 	B->mtx[3][2] = 7;
 	B->mtx[3][3] = 8;
 	printf("**************************************************\n");
-	t_matrix *c = mtx_multiply(A, B);
-	for (int i = 0; i < c->size; i++)
-	{
-		printf(" |");
-		for (int j = 0; j < c->size; j++)
-			printf("%.2f |", c->mtx[i][j]);
-		printf("\n");
-	}
+	// t_matrix *c = mtx_multiply(A, B);
+	// for (int i = 0; i < c->size; i++)
+	// {
+	// 	printf(" |");
+	// 	for (int j = 0; j < c->size; j++)
+	// 		printf("%.2f |", c->mtx[i][j]);
+	// 	printf("\n");
+	// }
+	// printf("**************************************************\n");
+	// t_matrix *a = identity_mtx(4);
+	// for (int i = 0; i < B->size; i++)
+	// {
+	// 	printf(" |");
+	// 	for (int j = 0; j < B->size; j++)
+	// 		printf("%.5f |", B->mtx[i][j]);
+	// 	printf("\n");
+	// }
 	printf("**************************************************\n");
-	t_matrix *a = identity_mtx(4);
+	t_matrix *a;
+	a = mtx_multiply(inverse(A), A);
 	for (int i = 0; i < a->size; i++)
 	{
 		printf(" |");
 		for (int j = 0; j < a->size; j++)
-			printf("%.2f |", a->mtx[i][j]);
+			printf("%.5f |", a->mtx[i][j]);
 		printf("\n");
 	}
+	// float f = minor(a, 0, 1);
+	// printf("\n******%.2f*****\n", f);
+/*
+| 3 | 5 | 0 |
+| 2 | -1 | -7 |
+| 6 | -1 | 5 |
+*/
+
 	// printf("**************************************************\n");
-	// t_matrix *C = mtx_transpose(A);
-	// for (int i = 0; i < 4; i++)
+	// t_matrix *C = mtx_create(3);
+	// C->mtx[0][0] = 3;
+	// C->mtx[0][1] = 5;
+	// C->mtx[0][2] = 0;
+	// C->mtx[1][0] = 2;
+	// C->mtx[1][1] = -1;
+	// C->mtx[1][2] = -7;
+	// C->mtx[2][0] = 6;
+	// C->mtx[2][1] = -1;
+	// C->mtx[2][2] = 5;
+	// for (int i = 0; i < C->size; i++)
 	// {
 	// 	printf(" |");
 	// 	for (int j = 0; j < C->size; j++)
 	// 		printf("%.2f |", C->mtx[i][j]);
 	// 	printf("\n");
 	// }
+	// float f = minor(C, 0, 0);
+	// printf("\nminor(C, 0, 0)******%.2f*****\n", f);
+	// float f1 = cofactor(C, 0, 0);
+	// printf("\ncofactor(C, 0, 0)******%.2f*****\n", f1);
+	// float f2 = minor(C, 1, 0);
+	// printf("\nminor(C, 1, 0)******%.2f*****\n", f2);
+	// float f3 = cofactor(C, 1, 0);
+	// printf("\ncofactor(C, 1, 0)******%.2f*****\n", f3);
 	return 0;
 }
